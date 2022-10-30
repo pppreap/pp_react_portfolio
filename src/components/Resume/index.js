78a66c6eb34e5c533ed551a9c1b2d7c9fa933ff3
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-29 22:59:17 -0400

    added resume download and reference section

---
+++ src/components/Resume/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Resume () {
+    return (
+        <section class='container'>
+            <h2 className='title'>Resume</h2>
+            <hr></hr>
+            <div class='mt-4'>
+                <h2 className='title'>Paula Preap</h2>
+                <ul>
+                    <li>
+                    Eager to develop my current skills to bring creative, beautiful, and responsive webpages to life.&#x2740;
+                    </li>
+                    <li>A quality professional with various experiences in industries from manufacturing, retail, administrative, to quality assurance. Will be an asset to team that’s inspiring and ambitious and thrive in a challenging work environment.</li>
+                </ul>
+                <a href='https://github.com/pppreap/pp-react-portfolio/raw/main/docs/PaulaPreapResume2022.docx' class='link'>
+                    Download Resume
+                </a>
+            </div>
+        </section>
+    );
+}
+
+export default Resume;
