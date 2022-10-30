e2f6a2a66bedaa9261611083b590b3c6a71ba5d4
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-29 23:07:20 -0400

    add helpers functions and utils folder

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { Route, Navigate, HashRouter } from 'react-router-dom';
 import Nav from '../../components/Nav';
 import About from '../../components/About';
-import Portfolio from '../components/Portfolio';
-import Contact from '../components/Contact';
-import Resume from './components/Contact';
+import Portfolio from '../../components/Portfolio';
+import Contact from '../../components/Contact';
+import Resume from '../../components/Resume';
 
 class Header extends Component {
     render(){
