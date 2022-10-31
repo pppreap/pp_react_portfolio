9a6efb73351a791f389423e2603fd37ec163a0a4
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-31 01:37:41 -0400

    styling font family

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,7 +11,7 @@ function ProjectCards(props) {
                 <p className='card-title'>{props.name}</p>
                 <p class='proj-icons-container'>
                 <a href={props.github}><img class='project-icon' src='https://img.icons8.com/fluent/48/000000/github.png' alt='Github Repo'/></a>
-                <a href={props.deploy}><img class='project-icon' src='https://img.icons8.com/fluent/48/000000/domain.png' alt='Live App'/></a>
+                <a href={props.deploy}><img class='project-icon-1' src='https://img.icons8.com/material-two-tone/452/launched-rocket.png' alt='Live App'/></a>
                 </p>
                 <p class="topics">
                     ({props.topics})
