581c8e861528e682ea3697195f6461f84f8fa2bf
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-31 02:29:25 -0400

    edit color for page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,31 +5,31 @@ import { NavLink } from 'react-router-dom';
 function Nav(props) {
     return (
         <div>
-            <Navbar expand='lg' bg='dark' sticky='top'>
+            <Navbar expand='lg' bg='light' sticky='top'>
             <NavLink className='nav-link' to='/'>
-                <div class='text-light'>
-                    <h4 class='nav-title-font'> Paula Preap</h4>
+                <div class='text-dark'>
+                    <h3 class='nav-title-font'> Paula Preap</h3>
                 </div>
                 </NavLink>
                 <ul class='navbar-nav ml-auto navitem-indent'>
                     <li class='nav-item'>
                         <NavLink to="/about">
-                            <div class='nav-font text-light'>About Me</div>
+                            <div class='nav-font text-dark'>About Me</div>
                         </NavLink>
                     </li>
                     <li class='nav-item'>
                         <NavLink to='/portfolio'>
-                            <div class='nav-font text-light'>Portfolio</div>
+                            <div class='nav-font text-dark'>Portfolio</div>
                         </NavLink>
                     </li>
                     <li class='nav-item'>
                         <NavLink to='/contact'>
-                            <div class='nav-font text-light'>Contact</div>
+                            <div class='nav-font text-dark'>Contact</div>
                         </NavLink>
                     </li>
                     <li class='nav-item'>
                         <NavLink to='/resume'>
-                            <div class='nav-font text-light'>Resume</div>
+                            <div class='nav-font text-dark'>Resume</div>
                         </NavLink>
                     </li>
                 </ul>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,18 +3,69 @@ import React from 'react';
 function Resume () {
     return (
         <section class='container'>
-            <h2 className='title'>Resume</h2>
+            <h2 className='title'> Paula's Resume</h2>
             <hr></hr>
             <div class='mt-5'>
-                <h2 className='title'>Paula Preap</h2>
+                <h4 className='title'><a href='https://github.com/pppreap/pp-react-portfolio/raw/main/docs/resume.docx' class='link'>
+                             Download Resume
+                </a></h4>
                 <ul>
                     <li>
                     Eager to develop my current skills to bring creative, beautiful, and responsive webpages to life.&#x2740;
                     </li>
                     <li>A quality professional with various experiences in industries from manufacturing, retail, administrative, to quality assurance. Will be an asset to team that’s inspiring and ambitious and thrive in a challenging work environment.</li>
-                </ul><a href='https://github.com/pppreap/pp-react-portfolio/raw/main/docs/resume.docx' class='link'>
-                             Download Resume
-                </a>
+                </ul>
+                
+            <hr />
+         <h2>Work Experience</h2>
+          <hr />
+<p>
+ QA DOCUMENTATION SPECIALIST • STC BIOLOGICS INC • NOV 2019-MAR 2022
+Perform all aspects of GMP documentation: issuing test methods, forms, batch records, logbooks, SOPs and archiving in accordance with the relevant site SOPs and cGMP requirements
+Maintain and organize Quality Records, training files and electronic list of SOPS/equipment securely and in a readily retrievable system
+Manage the calibration, PM, and pest control program
+Support sample and materials management, flexibility to accommodate manufacturing and QC schedule, quarantine and release material as needed
+Manage and track biennial review of quality documents and write, format and review quality documents/procedures
+	Address alarms/issues on Smart Vue monitoring system and backup generator and escalate high risk issues to management
+Support and train current employees on GDP, Quality Systems, and schedule training/meetings
+Conduct monthly walkthroughs, and help prepare/participate for internal and external audits, and batch disposition
+</p>
+<p>
+ADMINISTRATIVE ASSISTANT • STC BIOLOGICS INC • AUG2019 – MAR 2020
+Managing and ordering supplies for the laboratory and office
+Enter and create PO/invoices and bills into QuickBooks
+Help with onboarding new employee and assist in hiring/training new employees for the operations/front desk position.
+Contact vendors and shipping services for updates on pricing, order status, and availability
+Assist program directors with coordination of samples to be sent out for outsource testing and obtains quotes for material/calibrations services
+Manages and schedule calendar for meetings/interviews.
+</p>
+<p>
+OPERATOR 1 • ABBOTT (FORMERLY ST. JUDE MEDICAL) • MAR2016 – NOV 2017
+Follow GDP, GMP, and complied with safety guidelines to produced high quality medical heart catheters in a clean room environment.
+Operated various equipment, programs, and machinery such as microscopes 40x, ultrasonic/ HEPA air filter cleaner, zebra thermal transfer printer, SAP, Omnicure 1000, LUNA OBR reflectometer, and Instron tensile/compression test.
+Trained in many different stations such as pigtail splicing, lens potting, sheath assembly, pouching and trained new team member. 	
+Handle LAL samples and inspect pouched products for possible contaminants. 
+Maintain accurate records of product to ensure quality documents are traceable.
+</p>
+<p>
+GUEST SERVICE ASSISTANT/ STARBUCKS BARISTA • TARGET • JUN2013 – CURRENT
+Investigated and resolved any immediate customer inquiries and complaints in a timely and empathetic manner
+Trained new staff and assist store leaders with smooth opening and closing.
+</p>
+<p>
+STUDENT LIBRARY ASSISTANT • UMASS AMHERST LIBRARIES • FEB2010– OCT2013
+Assist patrons on locating books/media, viewing microfiche, and answering question on obtaining library card/printing.
+</p>
+<p>
+<h2>Education</h2>
+Bachelor Degree 2014   University of Massachusetts, Amherst
+</p>
+<p>
+<h2>Skills</h2>
+Microsoft Office , GMP,  GDP
+Goal Oriented, Effective Time Management  and Organization Skills, Highly Dependable Team Player,  Excellent Attention to Details, Strong Initiative and Fast Learner,  Embrace Culture of Compliance and Process Improvements
+</p>              
+
             </div>
         </section>
     );
