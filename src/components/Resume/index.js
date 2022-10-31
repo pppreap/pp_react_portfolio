a02d3308cbdf317b890166c2a92cc2743ca147ef
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-31 00:41:07 -0400

    added photo link to portfolio

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -12,7 +12,7 @@ function Resume () {
                     Eager to develop my current skills to bring creative, beautiful, and responsive webpages to life.&#x2740;
                     </li>
                     <li>A quality professional with various experiences in industries from manufacturing, retail, administrative, to quality assurance. Will be an asset to team that’s inspiring and ambitious and thrive in a challenging work environment.</li>
-                </ul><a href='https://github.com/pppreap/pp-react-portfolio/raw/main/docs/PaulaPreapResume2022.docx' class='link'>
+                </ul><a href='https://github.com/pppreap/pp-react-portfolio/raw/main/docs/resume.docx' class='link'>
                              Download Resume
                 </a>
             </div>
